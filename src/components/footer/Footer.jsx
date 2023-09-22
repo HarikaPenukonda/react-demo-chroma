939d11ee657b3507028a94479694c48902a34fb2
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-22 13:23:05 -0500

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import "./footer.css"
+
+export default function Footer(){
+    <div className="footer">
+        <span><h1>My website</h1></span>
+        <span>© Admin Dashboard</span>
+    </div>
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+import "./home.css"
+
+export default function Home(){
+    return(
+        <div className="home">
+            <div className="box box1">
+                <TopBox/>
+            </div>
+            <div className="box box2">
+                <ChartBox/>
+            </div>
+            <div className="box box3">
+                <ChartBox/>
+            </div>
+            <div className="box box4">
+                <PieChartBox/>
+            </div>
+            <div className="box box5">
+                <ChartBox/>
+            </div>
+            <div className="box box6">
+                <ChartBox/>
+            </div>
+            <div className="box box7">
+                <BigChartBox/>
+            </div>
+            <div className="box box8">
+                <BarChartBox/>
+            </div>
+            <div className="box box9">
+                <BarChartBox/>
+            </div>
+        </div>
+    )
+}
