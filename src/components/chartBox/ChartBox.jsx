ac410e890dfd67080a3597604cb0eadd233e3334
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-23 18:41:05 -0500

    chartbox

--- src/components/chartBox/ChartBox.jsx
+++ src/components/chartBox/ChartBox.jsx
@@ -1,15 +1,19 @@
-import Link from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
 import "./chartBox.css"
-import {Line,LineChar,ResponsiveContainer,Tooltip} from "recharts"
+import {Line,LineChart,ResponsiveContainer,Tooltip} from "recharts"
+import chartbox from "../../chart-box"
 
-export default function ChartBox(props){
+export default function ChartBox(){
     return(
         <div className="chart-box">
+        {chartbox.map(item =>(
             <div className="box-info">
-                <div className="title">
-                    <img src={props}/>
-                </div>
-            </div>
-        </div>
+            <div className='title'>
+            <img src={item.icon} alt=""/>
+            <span>{item.title}</span>
+        </div>  
+    </div>     
+        ))}     
+    </div>
     )
 }
