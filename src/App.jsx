1b32ff3a79fafe58ebacbe6a4e98d8f482a5a54e
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-23 13:38:44 -0500

    Home Page with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 export default function App(){
-    return(
-        <div className="main">
+        const Layout = () => {
+            return(
+            <div className="main">
             <Navbar/>
-        <div className='container'>
-        <div className='menu-container'>
-        {/* <Menu/> */}
+            <div className='container'>
+            <div className='menu-container'>
+            {/* <Menu/> */}
+            </div>
+            <div className='content-container'>
+            <Outlet/>
+            </div>
         </div>
-
-        </div>
-            <Footer/>
-        </div>
+        <Footer/>
+        </div> 
     )
+}
+
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Layout/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+            }
+        ]
+    }
+])
+
+return <RouterProvider router={router} />
 }
