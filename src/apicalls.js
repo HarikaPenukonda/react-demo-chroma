5e17e5ca68863e0fab431d372d258c3168d4fbbb
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-24 18:49:54 -0500

    exporting getTopDealUSers function

--- src/apicalls.js
+++ src/apicalls.js
@@ -2,7 +2,13 @@ import axios from "axios"
 
 const basesUrl = "http://localhost:3001/"
 
-async function getTopDealUsers(){
+/*
+    The axios.get() returns a promise, which returns a promise
+*/
+
+export async function getTopDealUsers(){
    const response =  await axios.get(`${baseURL}users/top-deal-users`)
    const responseBody = await response.json()
+   console.log(responseBody)
+   return responseBody
 }
