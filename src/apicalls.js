7ff9fd42f2465abad3424488b01d2f4f7ba6437d
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-24 19:13:36 -0500

    returning response data

--- src/apicalls.js
+++ src/apicalls.js
@@ -6,11 +6,12 @@ const baseURL = "http://localhost:3001/"
     The axios.get() returns a promise, which returns a promise
 */
 
+/*
+    response - {data: {…}, status: 200, statusText: 'OK', headers: AxiosHeaders, config: {…}, …}
+*/ 
+
 export async function getTopDealUsers(){
    const response =  await axios.get(`${baseURL}users/top-deal-users`)
-   //const responseBody = await response.json()
-   //console.log(responseBody)
-   //return responseBody
-   console.log(response)
-   return response
+   //console.log(response.data)
+   return response.data
 }
